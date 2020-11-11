ce0929f1f3c4179f5c5e6abe73ef3ad126aba7fc
philipwu62 <57893233+philipwu62@users.noreply.github.com>
2020-11-11 14:20:19 -0800

    Update video_widg.py

--- lib/video_widg.py
+++ lib/video_widg.py
@@ -268,7 +268,7 @@ def pynq_label_question(image):
     return
 
 def pop_images():
-    t = widgets.fixed(pynq_label_question,image=fixed(pynq_imgs))
+    t = widgets.interactive(pynq_label_question,image=fixed(pynq_imgs))
     return t
 
 def populate_questions():
