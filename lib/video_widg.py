bb766fcf7f6d21605a99d0843c365da68e08b31a
philipwu62 <57893233+philipwu62@users.noreply.github.com>
2020-11-12 13:12:53 -0800

    Update video_widg.py

--- lib/video_widg.py
+++ lib/video_widg.py
@@ -285,13 +285,11 @@ def populate_questions():
             question.append(platforms)
         if i == 20:
             img_choice = 0
-            platforms = widgets.Box(pynq_box)
-            #platforms = pop_images(img_choice)
+            platforms = pop_images(img_choice)
             question.append(platforms)
         if i == 21:
             img_choice = 1
-            platforms = widgets.Box(pynq_box)
-            #platforms = pop_images(img_choice)
+            platforms = pop_images(img_choice)
             question.append(platforms)
         question_list.append(question)
     return question_list
